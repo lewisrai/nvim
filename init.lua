a10c03560411ad80e8ded52889b1fd374f3a4a2e
lewisrai <144852757+lewisrai@users.noreply.github.com>
2024-05-07 02:28:00 +0100

    Gitsigns

--- init.lua
+++ init.lua
@@ -3,12 +3,14 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
 
+vim.opt.cursorline = true
 vim.opt.fileformats = "unix,dos"
 vim.opt.guicursor = ""
 vim.opt.laststatus = 3
 vim.opt.number = true
 vim.opt.pumheight = 12
 vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
 vim.opt.smartindent = true
 vim.opt.termguicolors = true
 vim.opt.wrap = false
@@ -30,9 +32,20 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
+vim.keymap.set("n", "q", function()
+	vim.api.nvim_exec(":nohl")
+end, {})
+
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
 	callback = function()
 		vim.lsp.buf.format()
+	end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
 	end,
 })
